e079e8cedc57e23c4f13d72e81cfb61bbe42e8fc
ahmed-oukacha <ahmed.ouka23456@gmail.com>
2025-09-01 19:41:32 +0200

    llm factory

--- src/help/config.py
+++ src/help/config.py
@@ -23,6 +23,20 @@ class Settings(BaseSettings):
     FILE_DEFAULT_CHUNK_SIZE: int = 512000
     FILE_MAX_SIZE: int = 10
 
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
+
     model_config = SettingsConfigDict(
         env_file=str(ENV_FILE_PATH), 
         env_file_encoding='utf-8',
