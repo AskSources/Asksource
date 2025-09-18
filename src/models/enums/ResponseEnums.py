ef67b648ef5a3e60ad70738307d384bd7c374905
Ahmed-oukacha <saad.ouka123456@gmail.com>
2025-09-18 04:01:02 +0200

    test3

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class ResponseSignal(Enum):
 
-    FILE_VALIDATED_SUCCESS = "file_validate_successfully"
+    FILE_VALIDATED_SUCCESS = "file-validate_successfully"
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCCESS = "file_upload_success"
